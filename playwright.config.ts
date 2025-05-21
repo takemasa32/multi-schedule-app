8a9c3f37c47f51a1ef428dfc3044358926b834cd
takemasa32 <takemasa0302@gmail.com>
2025-05-21 16:07:21 +0900

    E2Eテストの動作を少し修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: 'http://localhost:3000',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     trace: 'on-first-retry',
     video: 'on',
